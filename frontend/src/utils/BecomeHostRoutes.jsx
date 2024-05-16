6a87befd53e0b07c29398e364eacf242d2d6af29
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-05-16 14:49:33 +0530

    Fix : The routing fixed.
    But it causes the issue.

--- frontend/src/utils/BecomeHostRoutes.jsx
+++ frontend/src/utils/BecomeHostRoutes.jsx
@@ -6,11 +6,29 @@ const BecomeHostRoutes = ({ component: Component, ...rest }) => {
   const user = useSelector((state) => state.user.userDetails);
   console.log("The becomeHostRoutes called,", user);
 
+  const navigate = useNavigate();
+
   if (user) {
-    if (!user.emailVerification.verified || !user.mobileVerification.verified) {
-      toast.error(
+    if (
+      user.emailVerification &&
+      user.mobileVerification &&
+      !user.emailVerification.verified &&
+      !user.mobileVerification.verified
+    ) {
+      const toastId = toast.error(
         "Please verify your email and mobile number before proceeding further."
       );
+
+      setTimeout(() => {
+        toast.dismiss(toastId);
+        const redirectToastId = toast.success(
+          "Redirecting for verification..."
+        );
+        setTimeout(() => {
+          toast.dismiss(redirectToastId);
+          navigate(`/users/show/${user?._id}/verify-account`);
+        }, 1500);
+      }, 2500);
       return <Navigate to="/host/rooms" />;
     } else {
       return <Component {...rest} />;
